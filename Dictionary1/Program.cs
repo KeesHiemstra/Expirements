c9623019cfcade295b8d969c8ba90377929d07e9
Kees Hiemstra <chi@xs4all.nl>
2018-06-13 11:47:37 +0200

    Added Dictionary

---
+++ Dictionary1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary1
+{
+	public class Program
+	{
+		public static readonly IDictionary<string, string> Dict = new Dictionary<string, string>();
+
+		static void Main(string[] args)
+		{
+			Initilize();
+
+			foreach (var item in Dict)
+			{
+				Console.WriteLine($"{item.Key, 6} {item.Value}");
+			}
+
+			Console.WriteLine();
+
+			Console.WriteLine($"21°C ({ Dict["°C"] })");
+
+			Console.WriteLine();
+			Console.Write("Press any key...");
+			Console.ReadKey();
+		}
+
+		#region Initilize Dict dictionary
+		public static void Initilize()
+		{
+			//Degree Sign = Alt+0176
+			Dict.Add("Bft", "Beaufort");
+			Dict.Add("K", "Kelvin");
+			Dict.Add("°C", "degree Celcius");
+			Dict.Add("°F", "degree Fahrenheit");
+			Dict.Add("km/h", "kilometer per hour");
+			Dict.Add("m/s", "meter per second");
+		}
+		#endregion
+	}
+}
--- Logging1/Program.cs
+++ Logging1/Program.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
 
 namespace Logging1
 {
 	class Program
 	{
+
+		private readonly ILogger _logger;
+
+		public Program() => _logger = new LoggerFactory()
+							//.AddConsole(LogLevel.Information)
+							//.AddDebug(LogLevel.Debug)
+							.CreateLogger<Program>();
+
 		static void Main(string[] args)
 		{
-
+			_logger.LogInformation($"Retrieving samples from '{nameof(Main)}'");
 
 			Console.WriteLine();
 			Console.Write("Press any key...");
