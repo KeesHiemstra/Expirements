ca0b057bbe400e0c5a61c7ef1ceb553cf67a46e4
Kees Hiemstra <chi@xs4all.nl>
2019-03-31 19:53:28 +0200

    GetPeoperties

---
+++ GetType1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetType1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass MyObject = new MyClass { LastName = "Hiemstra", FirstName = "Kees", Age = 50 };
+
+			Console.WriteLine(MyObject.GetType());
+
+			Console.WriteLine(MyObject.GetType().GetFields());
+			foreach (var item in MyObject.GetType().GetFields())
+			{
+				Console.WriteLine(item.FieldType);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine(MyObject.GetType().GetMembers());
+			foreach (var item in MyObject.GetType().GetMembers())
+			{
+				Console.WriteLine($"{item.MemberType} = {item.Name} ({item.ToString()})");
+			}
+			Console.WriteLine();
+
+			Console.WriteLine(MyObject.GetType().GetProperties());
+			foreach (var item in MyObject.GetType().GetProperties())
+			{
+				Console.WriteLine($"{item.Name}: {item.GetValue(MyObject)}");
+			}
+
+
+			Console.Write("\nPress any key...");
+			Console.ReadKey();
+		}
+	}
+
+	public class MyClass
+	{
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public int Age { get; set; }
+	}
+}
---
+++ String1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1
+{
+  class Program
+  {
+    public enum Bits
+    {
+      x86, x64
+    }
+
+    const string Key32 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+    const string Key64 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+    static void Main(string[] args)
+    {
+      WriteSoftwareSource(false);
+
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+
+    private static void WriteSoftwareSource(bool Bits64)
+    {
+      string[] strings = new string[2];
+      strings[0] = Key32;
+      strings[1] = Key64;
+
+      Console.WriteLine(strings[0]);
+
+      Console.WriteLine(Bits.x64.ToString());
+    }
+  }
+}
