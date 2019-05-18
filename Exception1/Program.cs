8fb5dcaedffbaa618dedd451672233961fab2f53
Kees Hiemstra <chi@xs4all.nl>
2019-05-18 06:29:54 +0200

    Renamed

--- Exception1/Program.cs
+++ Exception1/Program.cs
@@ -5,17 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace String2
+namespace Exception1
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			string Line = "This is a line";
-			Console.WriteLine(Line.Substring(0, 6));
-			Console.WriteLine(Line.PadLeft(16));
-
-			Console.WriteLine(Line);
+			try
+			{
+                throw new AccessViolationException();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"{ex.Source}: 0x{ex.HResult:X}\n{ex.Message}");
+			}
 
 			Console.Write("\nPress any key");
 			Console.ReadKey();
