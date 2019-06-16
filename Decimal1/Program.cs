c840929c2a62fe49c75c67d469acca0320500891
Kees Hiemstra <chi@xs4all.nl>
2019-06-16 16:42:49 +0200

    Strange decimal.Parse

---
+++ Decimal1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"1.00 => {decimal.Parse("1.00")} / 1,00 {decimal.Parse("1,00")}");
+
+			bool DivideBy100 = (decimal.Parse("1.25") == 125);
+
+			string Record = "5,25";
+			decimal Amount = decimal.Parse(Record.Replace(',', '.'));
+			if (DivideBy100)
+			{
+				Console.WriteLine("Divide by 100");
+				Amount /= 100;
+			}
+
+			Console.WriteLine(Amount);
+
+			Console.WriteLine("\nPress any key...");
+			Console.ReadKey();
+		}
+	}
+}
