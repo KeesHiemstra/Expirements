31bd035250ef36195314a8a19e5ddd1c9e8c12ac
Kees Hiemstra <chi@xs4all.nl>
2019-06-17 19:28:19 +0200

    Add Logging2

---
+++ Logging2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(Log("Start message")); 
+
+			Console.Write("\nPress any key...");
+			Console.ReadKey();
+
+		}
+
+		static string Log(string message)
+		{
+
+			string LogFileName = Assembly.GetEntryAssembly().Location.Replace(".exe", ".log");
+
+			using (StreamWriter stream = new StreamWriter(LogFileName, true))
+			{
+				stream.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {message}");
+			}
+
+			return message;
+
+		}
+
+	}
+}
