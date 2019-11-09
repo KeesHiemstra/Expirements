e62e1e2a39d200da05f8b480d2c9e4b2a70d0be5
Kees Hiemstra <chi@xs4all.nl>
2019-11-09 12:18:51 +0100

    DateTime1 calculate - month

---
+++ DateTime1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DateTime date = new DateTime(2019, 3, 31);
+
+      Console.WriteLine($"{date} - 1 month = {date.AddMonths(-1)}");
+
+      Console.WriteLine("\nPress any key...");
+      Console.ReadKey();
+    }
+  }
+}
